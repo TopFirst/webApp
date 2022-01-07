a05a0dbb1e2576ac0467302dd6dee3141e2a9580
TopFirst <topfirst4@gmail.com>
2022-01-07 12:06:03 +0700

    ubah sesuai tampilan depan dan tambahkan permission untuk pagecontroller

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,12 +8,20 @@
     <div class="col-xl-8 stretch-card grid-margin">
       <div class="position-relative">
         <div style="max-height: 820px; overflow:hidden;">
-        <img
-          src="{{ asset('uploads/' . $posts[0]->post_thumbnail) }}"
-          alt="banner"
-          class="img-fluid rounded"
-        />
+          <a href="{{ route('posts.lihat',$posts[0]->post_slug) }}" class="text-decoration-none text-dark">
+            <img
+              src="{{ asset('uploads/' . $posts[0]->post_thumbnail) }}"
+              alt="banner"
+              class="img-fluid rounded"
+            />
+          </a>
         </div>
+        @if ($posts[0]->tipe->post_type_slug=='video')
+          <div style="top:50%; left:50%; position:absolute; transform:translate(-50%, -50%)">
+            <a href="{{ route('posts.lihat',$posts[0]->post_slug) }}" class="text-decoration-none text-danger"><i class="mdi mdi-youtube" style="font-size: 70px;" aria-hidden="true"></i></a>
+          </div>
+        @endif
+        
         <div class="banner-content">
           <div class="badge badge-danger fs-12 font-weight-bold mb-3">
             <a href="{{ route('posts.daftar') }}?category={{ $posts[0]->kategori->category_slug }}" class="text-decoration-none text-white">{{ $posts[0]->kategori->category_name??'-' }}</a>
@@ -48,11 +56,13 @@
                 </div>
               </div>
               <div class="rotate-img">
-                <img
-                  src="{{ asset('uploads/' . $post->post_thumbnail) }}"
-                  alt="{{ $post->post_thumbnail }}"
-                  class="img-fluid img-lg"
-                />
+              <a href="{{ route('posts.lihat',$post->post_slug) }}" class="text-decoration-none text-dark">
+                    <img
+                      src="{{ asset('uploads/' . $post->post_thumbnail) }}"
+                      alt="{{ $post->post_thumbnail }}"
+                      class="img-fluid img-lg"
+                    />
+              </a>
               </div>
             </div>
           @endforeach
@@ -81,11 +91,13 @@
             <div class="col-sm-4 grid-margin">
               <div class="position-relative">
                 <div class="rotate-img">
-                  <img
-                    src="{{ asset('uploads/' . $post->post_thumbnail) }}"
-                    alt="thumb"
-                    class="img-fluid"
-                  />
+                <a href="{{ route('posts.lihat',$post->post_slug) }}" class="text-decoration-none text-dark">
+                    <img
+                      src="{{ asset('uploads/' . $post->post_thumbnail) }}"
+                      alt="thumb"
+                      class="img-fluid"
+                    />
+                </a>
                 </div>
                 <div class="badge-positioned">
                   <span class="badge badge-danger font-weight-bold"><a href="{{ route('posts.daftar') }}?category={{ $post->kategori->category_slug }}" class="text-decoration-none text-white">{{ $post->kategori->category_name }}</a></span>
@@ -157,11 +169,13 @@
                 <div class="d-flex justify-content-between align-items-center border-bottom pb-3">
                   <div class="div-w-80 mr-3">
                     <div class="rotate-img">
-                      <img
-                        src="{{ asset('uploads/' . $post->post_thumbnail) }}"
-                        alt="thumb"
-                        class="img-fluid"
-                      />
+                      <a href="{{ route('posts.lihat',$post->post_slug) }}" class="text-decoration-none text-dark">
+                        <img
+                          src="{{ asset('uploads/' . $post->post_thumbnail) }}"
+                          alt="thumb"
+                          class="img-fluid"
+                        />
+                      </a>
                     </div>
                   </div>
                   <h3 class="font-weight-600 mb-0">
@@ -188,11 +202,13 @@
               <div class="row">
                 <div class="col-xl-6 col-lg-8 col-sm-6">
                   <div class="rotate-img">
-                    <img
-                      src="{{ asset('uploads/' . $posts[0]->post_thumbnail) }}"
-                      alt="thumb"
-                      class="img-fluid"
-                    />
+                    <a href="{{ route('posts.lihat',$posts[0]->post_slug) }}" class="text-decoration-none text-dark">
+                      <img
+                        src="{{ asset('uploads/' . $posts[0]->post_thumbnail) }}"
+                        alt="thumb"
+                        class="img-fluid"
+                      />
+                    </a>
                   </div>
                   <h2 class="mt-3 text-primary mb-2">
                     <a href="{{ route('posts.lihat',$posts[0]->post_slug) }}" class="text-decoration-none text-dark">{{ $posts[0]->post_title }}</a>
@@ -239,11 +255,13 @@
                   @endphp 
                     <div class="border-bottom pb-3">
                       <div class="rotate-img">
-                        <img
-                          src="{{ asset('uploads/' . $post->post_thumbnail) }}"
-                          alt="thumb"
-                          class="img-fluid"
-                        />
+                        <a href="{{ route('posts.lihat',$post->post_slug) }}" class="text-decoration-none text-dark">
+                          <img
+                            src="{{ asset('uploads/' . $post->post_thumbnail) }}"
+                            alt="thumb"
+                            class="img-fluid"
+                          />
+                        </a>
                       </div>
                       <p class="fs-16 font-weight-600 mb-0 mt-3">
                         <a href="{{ route('posts.lihat',$post->post_slug) }}" class="text-decoration-none text-dark">{{ $post->author->name }}: {{ $first_part }}</a>
@@ -273,11 +291,13 @@
                         <div class="row">
                           <div class="col-sm-5 pr-2">
                             <div class="rotate-img">
-                              <img
-                                src="{{ asset('uploads/' . $post->post_thumbnail) }}"
-                                alt="thumb"
-                                class="img-fluid w-100"
-                              />
+                              <a href="{{ route('posts.lihat',$post->post_slug) }}" class="text-decoration-none text-dark">
+                                <img
+                                  src="{{ asset('uploads/' . $post->post_thumbnail) }}"
+                                  alt="thumb"
+                                  class="img-fluid w-100"
+                                />
+                              </a>
                             </div>
                           </div>
                           <div class="col-sm-7 pl-2">
